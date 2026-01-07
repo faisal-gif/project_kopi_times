8f7825a0b3c2f1baff028165679bdd8794dc81b7
faisal-gif <fagdera@gmail.com>
2026-01-07 12:23:57 +0700

    update meta landing

--- resources/js/Pages/Welcome/Index.jsx
+++ resources/js/Pages/Welcome/Index.jsx
@@ -14,13 +14,28 @@ export default function Index({ newsPackages }) {
     return (
         <>
             <Head>
-                <title>Beranda</title>
+                {/* Standard Meta Tags */}
+                <title>Kopi TIMES</title>
                 <meta name="description" content="Membership Penulis Kopi TIMES adalah program keanggotaan bagi penulis yang ingin terlibat aktif dalam ekosistem gagasan di TIMES Indonesia." />
+                <meta name="keywords" content="Kopi TIMES, TIMES Indonesia, Penulis, Membership, Jurnalisme Positif" />
+                <link rel="canonical" href={domain} />
 
-                {/* Meta untuk Social Media (Open Graph) */}
-                <meta property="og:title" content="Beranda - Kopi Times" />
-                <meta property="og:description" content="Membership Penulis Kopi TIMES adalah program keanggotaan bagi penulis yang ingin terlibat aktif dalam ekosistem gagasan di TIMES Indonesia. " />
-                <meta property="og:image" content="/logo-web.png" />
+                {/* Facebook / Open Graph (WhatsApp juga pakai ini) */}
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content={domain} />
+                <meta property="og:title" content="Beranda - Kopi TIMES" />
+                <meta property="og:description" content="Membership Penulis Kopi TIMES adalah program keanggotaan bagi penulis yang ingin terlibat aktif dalam ekosistem gagasan di TIMES Indonesia." />
+                {/* PENTING: Gunakan URL Absolut agar gambar muncul */}
+                <meta property="og:image" content={"/logo-web.png"} />
+                <meta property="og:image:width" content="1200" />
+                <meta property="og:image:height" content="630" />
+
+                {/* Twitter / X Meta Tags */}
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:url" content={domain} />
+                <meta name="twitter:title" content="Beranda - Kopi TIMES" />
+                <meta name="twitter:description" content="Membership Penulis Kopi TIMES adalah program keanggotaan bagi penulis yang ingin terlibat aktif dalam ekosistem gagasan di TIMES Indonesia." />
+                <meta name="twitter:image" content={"/logo-web.png"} />
             </Head>
             <LandingLayout>
                 <HeroSection />
