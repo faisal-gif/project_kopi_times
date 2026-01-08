f7eb55cbd2d0e9ebb37c95346463abe687b0ecbb
faisal-gif <fagdera@gmail.com>
2026-01-08 17:26:26 +0700

    modified:   resources/js/Pages/Dashboard.jsx

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -27,7 +27,7 @@ export default function Dashboard({ total_news, pending_news, publish_news }) {
                         <div className="flex items-center justify-between text-lg">
                             <div className="flex items-center gap-2">
                                 <Calendar className="w-5 h-5 text-primary" />
-                                Masa Berlaku Paket
+                                Status Membership
                             </div>
                         </div>
 
@@ -45,32 +45,7 @@ export default function Dashboard({ total_news, pending_news, publish_news }) {
                     </Card>
 
                     {/* Quota Card */}
-                    <Card className="border-primary/20 bg-linear-to-bl from-primary/10 to-accent/20">
-
-                        <div className="card-title flex items-center gap-2 text-lg">
-                            <TrendingUp className="w-5 h-5 text-primary" />
-                            Kuota Artikel Bulan Ini
-                        </div>
-
-
-                        <div className="space-y-4">
-                            <div className="flex justify-between text-sm">
-                                <span className="text-muted-foreground">Terpakai</span>
-                                <span className="font-semibold text-foreground">
-                                    {/* {writerData.quotaUsed} / {writerData.quotaLimit}  */}
-                                    {user.quota_news} artikel
-                                </span>
-                            </div>
-
-                            <p className="text-sm text-muted-foreground">
-                                Sisa {user.quota_news} artikel sampai {formatDate(user.dateexp)}.
-                                {/* {quotaPercentage >= 80 && (
-                                    <span className="text-orange-600 font-medium"> Kuota hampir habis!</span>
-                                )} */}
-                            </p>
-                        </div>
-
-                    </Card>
+                 
                 </div>
             </div>
 
