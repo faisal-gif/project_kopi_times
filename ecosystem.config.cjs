c20648f8300d7f8dc04b17be87b2eab6fc25a3e4
faisal-gif <fagdera@gmail.com>
2026-01-10 15:59:32 +0700

    modified:   ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,15 +1,15 @@
 module.exports = {
   apps: [
     {
-      name: "worker-kopi",
+      name: "kopi-worker",
       script: "artisan",
       args: "queue:work --sleep=3 --tries=3 --max-time=3600",
       interpreter: "php",
       instances: 1,
       autorestart: true,
     },
-    {
-      name: "ssr-kopi",
+   {
+      name: "kopi-ssr",
       script: "artisan",
       args: "inertia:start-ssr",
       interpreter: "php",
