d05b294c7c34b0035de17586c6955631fb009a3b
faisal-gif <fagdera@gmail.com>
2026-01-13 20:05:32 +0700

    modified:   resources/js/Layouts/AuthenticatedLayout.jsx

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -109,7 +109,7 @@ export default function AuthenticatedLayout({ header, children }) {
                             className={linkClass(isActive('news.*'))}
                         >
                             <NewspaperIcon size={16} />
-                            News
+                            Opini
                         </Link>
                     </li>
                     <li>
