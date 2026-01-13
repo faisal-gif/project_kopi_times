7ed7ad5e0f7e713c33eea865133cd80e8b4b1a55
faisal-gif <fagdera@gmail.com>
2026-01-13 13:35:35 +0700

    modified:   resources/js/Pages/Subscription/Index.jsx

--- resources/js/Pages/Subscription/Index.jsx
+++ resources/js/Pages/Subscription/Index.jsx
@@ -127,12 +127,7 @@ function Index({ newsPackages, newsSatuan, userPackage }) {
                         <div className='flex flex-col gap-4'>
                             <h2 className='text-2xl font-bold'>Paket</h2>
                             <div
-                                className={`grid gap-8 max-w-5xl mx-auto
-                            ${newsPackages.length === 1
-                                        ? "grid-cols-1 justify-items-center"
-                                        : "grid-cols-1 md:grid-cols-3"
-                                    }
-                `}>
+                                className={`grid gap-8 max-w-5xl mx-auto grid-cols-1 md:grid-cols-3`}>
                                 {newsPackages.map((plan) => (
                                     <Card
                                         key={plan.name}
