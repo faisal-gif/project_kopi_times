b5c5cbd78cc5feb013d85e10e5995a0e56b226ca
faisal-gif <fagdera@gmail.com>
2026-01-13 12:56:31 +0700

    modified:   app/Http/Controllers/SubscriptionController.php

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -11,14 +11,18 @@ class SubscriptionController extends Controller
 {
     public function index()
     {
-        $newsPackages = NewsPackage::where('type', '4')->where('level', 2)->get();
+        $newsPackages = NewsPackage::where('type', '4')->where('level', 2)->where('kategori_produk', 'paket')->get();
+        $newsSatuan = NewsPackage::where('type', '4')->where('level', 2)->where('kategori_produk', 'satuan')->get();
+
         $user = Auth::user();
 
         $userPackage = NewsPackage::find($user->package_id);
 
 
+
         return Inertia::render('Subscription/Index', [
             'newsPackages' => $newsPackages,
+            'newsSatuan' => $newsSatuan,
             'userPackage' => $userPackage,
         ]);
     }
