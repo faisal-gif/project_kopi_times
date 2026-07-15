f4c8d284cea2ed43be12e94f1225cd055bbd5111
faisal-gif <fagdera@gmail.com>
2026-07-15 11:10:58 +0700

    feat: add feed_instagram and ekoran updates to user on package purchase

--- app/Http/Controllers/TripayCallbackController.php
+++ app/Http/Controllers/TripayCallbackController.php
@@ -72,6 +72,8 @@ class TripayCallbackController extends Controller
                 }
 
                 $user->quota_news += $newsPackage->quota;
+                $user->feed_instagram += $newsPackage->feed_instagram;
+                $user->ekoran += $newsPackage->ekoran;
                 $user->dateexp = $baseDate;
                 $user->package_id = $newsPackage->id;
                 $user->status = 1;
