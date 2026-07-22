eef4a48f5bf7c72a78dbd70f37fb2cb270fae393
faisal-gif <fagdera@gmail.com>
2026-07-22 11:49:47 +0700

    fix: correct syntax error in merchandise shipments migration

--- database/migrations/2026_07_22_111916_create_merchandise_shipments_table.php
+++ database/migrations/2026_07_22_111916_create_merchandise_shipments_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('merchandise_shipments', function (Blueprint $table) {
-            $$table->id();
+            $table->id();
             
             // Relasi ke tabel wartawan (penerima)
             // Sesuaikan references('id')->on('wartawan') jika menggunakan custom table
